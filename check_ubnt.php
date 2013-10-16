902a351ca89876636c2fac7c4e418b4a0197aef6
Zig Fisher <flyrouter@gmail.com>
2013-10-16 13:49:44 +0300

    up

--- check_ubnt.php
+++ check_ubnt.php
@@ -127,23 +127,13 @@ $def[5] .= "GPRINT:lavg:AVERAGE:'%7.2lf %S avg' ";
 $def[5] .= "GPRINT:lavg:MAX:'%7.2lf %S max' ";
 $def[5] .= "GPRINT:lavg:MIN:'%7.2lf %S min'\\n ";
 
-
 $ds_name[6] = "Uptime";
 $opt[6] = "--vertical-label 'uptime, sec' --title '{$this->MACRO['DISP_HOSTNAME']}' --lower-limit=0 ";
 $def[6] = "DEF:uptime={$_UPTIME['RRDFILE']}:{$_UPTIME['DS']}:AVERAGE ";
-$def[6] .= "AREA:uptime{$_C_USERS}:'sec' ";
-$def[6] .= "GPRINT:uptime:LAST:'%7.2lf %S last' ";
+##$def[6] .= "CDEF:days=uptime,86400,/ ";
+$def[6] .= "AREA:uptime{$_C_UPTIME}:'sec' ";
+$def[6] .= "GPRINT:uptime:LAST:'%7.2lf' ";
 $def[6] .= "GPRINT:uptime:AVERAGE:'%7.2lf %S avg' ";
 $def[6] .= "GPRINT:uptime:MAX:'%7.2lf %S max' ";
-
-
-#$ds_name[6] = "Uptime";
-#$opt[6] = "--vertical-label 'uptime, sec' --title '{$this->MACRO['DISP_HOSTNAME']}' --lower-limit=0  ";
-#$def[6] = "DEF:uptime={$_UPTIME['RRDFILE']}:{$_UPTIME['DS']}:AVERAGE ";
-##$def[6] .= "CDEF:days=uptime,86400,/ ";
-#$def[6] .= "AREA:uptime{$_C_UPTIME}:'sec' ";
-#$def[6] .= "GPRINT:uptime:LAST:'%7.2lf %S last' ";
-#$def[6] .= "GPRINT:uptime:AVERAGE:'%7.2lf %S avg' ";
-#$def[6] .= "GPRINT:uptime:MAX:'%7.2lf %S max'\\n ";
 
 ?>
